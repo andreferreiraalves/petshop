31b75701b4793b5fa4ad72c1fe5bd4015055e7ac
Andre Ferreira <andre.ferreira@mercatustecnologia.com.br>
2019-12-03 14:15:00 -0300

    configuração de rotas

---
+++ src/app/pages/master/frame.page.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-frame-page',
+    template: '<app-nav-bar></app-nav-bar><router-outlet></router-outlet>'
+})
+export class FramePageComponent { }
